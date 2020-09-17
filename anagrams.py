311b22f4563c2436d2595c7d1254797507264352
Arianna Basha <ariannagbasha@gmail.com>
2020-09-17 14:21:27 -0500

    test_long, test_short, and flake8 passed all tests

--- anagrams.py
+++ anagrams.py
@@ -10,7 +10,8 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = """The Gabster(Gabby) collabs:
+                Shani The Awesome and Sondos The Great"""
 
 import sys
 
@@ -27,11 +28,10 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {alphabetize(word): [] for word in words}
+    for word in words:
+        if alphabetize(word) in anagrams:
+            anagrams[alphabetize(word)].append(word)
     return anagrams
 
 
